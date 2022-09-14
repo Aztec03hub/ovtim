7a63e90650e9b1654295b75405fd2b2c9ec2f371
Aztec03hub <pjl@mhelectricmotors.com>
2022-09-13 22:04:07 -0500

    Updated basic tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,18 @@ def test_managescanners():
     response = client.get("/managescanners", headers={"content-type": "text/html; charset=utf-8"})
     assert response.status_code == 200
     assert b"Manage Scanners - Omron Microscan VT-Interface Module" in response.content
+
+def test_weblink():
+    response = client.get("/weblink", headers={"content-type": "text/html; charset=utf-8"})
+    assert response.status_code == 200
+    assert b"Weblink - Omron Microscan VT-Interface Module" in response.content
+
+def test_help():
+    response = client.get("/help", headers={"content-type": "text/html; charset=utf-8"})
+    assert response.status_code == 200
+    assert b"Help - Omron Microscan VT-Interface Module" in response.content
+
+def test_settings():
+    response = client.get("/settings", headers={"content-type": "text/html; charset=utf-8"})
+    assert response.status_code == 200
+    assert b"Settings - Omron Microscan VT-Interface Module" in response.content
