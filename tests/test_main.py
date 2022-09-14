b4d53e45a12911fd2edf9948edacee24314ab417
Aztec03hub <pjl@mhelectricmotors.com>
2022-09-13 21:26:41 -0500

    Added extremely basic tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,15 +5,13 @@ client = TestClient(app)
 
 
 def test_home():
-    response = client.get(
-        "/", headers={"content-type": "text/html; charset=utf-8"})
+    response = client.get("/", headers={"content-type": "text/html; charset=utf-8"})
     assert response.status_code == 200
-    assert b"Welcome to FastAPI Starter" in response.content
+    assert b"Omron Microscan VT-Interface Module" in response.content
     response = client.get("/static/css/style3.css")
     assert response.status_code == 200
 
 def test_managescanners():
-    response = client.get("/managescanners",
-                          headers={"content-type": "text/html; charset=utf-8"})
+    response = client.get("/managescanners", headers={"content-type": "text/html; charset=utf-8"})
     assert response.status_code == 200
-    assert b"Manage Scanners" in response.content
+    assert b"Manage Scanners - Omron Microscan VT-Interface Module" in response.content
